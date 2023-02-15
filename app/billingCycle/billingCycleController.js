8b19a88e25bbd2ab648de18f15641a8226a54047
Rogerio Cordeiro <rocordeirosilva@gmail.com>
2023-02-15 16:47:41 -0300

    Aula 89 Msg Error e Correção Angula 1.6

--- app/billingCycle/billingCycleController.js
+++ app/billingCycle/billingCycleController.js
@@ -1,17 +1,20 @@
 (function() {
   angular.module('primeiraApp').controller('BillingCycleCtrl', [
     '$http',
+    'msgs',
     BillingCycleController
   ])
 
-  function BillingCycleController($http) {
+  function BillingCycleController($http, msgs) {
     const vm = this
 
     vm.create = function() {
       const url ='http://localhost:3003/api/billingCycles'
       $http.post(url, vm.billingCycle).then(function(response) {
         vm.billingCycle = {}
-        console.log('Sucesso!')
+        msgs.addSucess('Operação realizada com sucesso!!')
+      }).catch(function(response) {
+        msgs.addError(response.data.errors)
       })
     }
   }
