c0e16257c607c98269c516f33fc555f8d6b15a66
Rogerio Cordeiro <rocordeirosilva@gmail.com>
2023-02-15 16:56:24 -0300

    Aula 90 Lista de pagamentos Parte 2

--- app/billingCycle/billingCycleController.js
+++ app/billingCycle/billingCycleController.js
@@ -7,11 +7,17 @@
 
   function BillingCycleController($http, msgs) {
     const vm = this
+    const url ='http://localhost:3003/api/billingCycles'
+
+    vm.refresh = function() {
+      $http.get(url).then(function(response){
+        vm.billingCycle = {}
+        vm.billingCycles = response
+      })
+    }
 
     vm.create = function() {
-      const url ='http://localhost:3003/api/billingCycles'
       $http.post(url, vm.billingCycle).then(function(response) {
-        vm.billingCycle = {}
         msgs.addSucess('Operação realizada com sucesso!!')
       }).catch(function(response) {
         msgs.addError(response.data.errors)
