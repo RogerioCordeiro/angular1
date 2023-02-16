4a804598c889711d3e919203334a2d14f21207bb
Rogerio Cordeiro <rocordeirosilva@gmail.com>
2023-02-15 22:05:34 -0300

    Aula 93 Botões Update e Delete

--- app/billingCycle/billingCycleController.js
+++ app/billingCycle/billingCycleController.js
@@ -26,6 +26,18 @@
         msgs.addError(response.data.errors)
       })
     }
+
+
+    vm.showTabUpdate = function(billingCycle) {
+      vm.billingCycle = billingCycle
+      tabs.show(vm, {tabUpdate: true})
+    }
+
+    vm.showTabDelete = function(billingCycle){
+      vm.billingCycle = billingCycle
+      tabs.show(vm, {tabDelete: true})
+    }
+
     vm.refresh()
   }
   
